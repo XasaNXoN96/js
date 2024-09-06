b5b475dc16c602b74c6b4bbce1741f774ad9d7fb
XasaNXoN96 <Zikk.1996@mail.ru>
2024-09-06 15:46:50 +0500

    js file qoshildi

---
+++ app.js
@@ -0,0 +1,34 @@
+//string
+let name = 'ism';
+console.log(name);
+
+//number
+let a = 4;
+let b = 6;
+console.log(a + b);
+
+
+//bollean
+
+let x = true;
+console.log(x);
+
+let y = false;
+console.log(y);
+console.log(typeof y);
+
+
+//undifined
+let word;
+console.log(word);
+
+//null
+const word2 = null;
+console.log(word2);
+
+
+let m = 10;
+let n = 11
+
+console.log(m == n);
+console.log(m === n);
